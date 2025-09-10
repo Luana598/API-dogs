d1589889aad802074e377e1f162d4f3f50b0651c
LUANA MARIANA LOPES BOMFIM <25132776@CFP127ALUNO.LOCAL>
2025-09-10 11:17:39 -0300

    imagens adicionadas à tela

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
 'use strict'
 
+const botaoBuscar = document.getElementById('buscar')
+const input = document.getElementById('inputRaca')
+const galeria = document.getElementById('galeria')
+
 // fazendo uma função assíncrona para obter as imagens no momento certo
 //await: faz com que o 'console.log' espere o tempo de carregamento da API
 // response = resposta possível do servidor, nesse caso, pending, pq não houve tempo para responder sem o async
 
-async function buscarImagens (raca){
+async function buscarImagens(raca) {
     const url = `https://dog.ceo/api/breed/${raca}/images`
-    const response = await fetch (url) 
+    const response = await fetch(url)
     const imagens = await response.json()
-    console.log (imagens.message)
+
+    console.log(imagens.message)
     return imagens.message
-} 
-
-
-
-function criarImagem(srcImagem) {
-    const input = document.getElementById('galeria')
-    const imagem = document.createElement('img')
-    const legenda = document.createElement('div')
-    const texto = document.createElement('label')
-
-    imagem.src = srcImagem.url
-    texto.textContent = srcImagem.nome
-
-    galeria.appendChild(imagem)
-    galeria.appendChild(legenda)
-    legenda.appendChild(texto)
-
 }
-buscarImagens("doberman")
+
+async function exibirImagens() {
+    const galeria = document.getElementById("galeria")
+    const raca = document.getElementById('inputRaca').value.toLowerCase()
+
+    galeria.innerHTML = "" // limpa as imagens antigas
+
+    const listaImg = await buscarImagens(raca)
+
+    listaImg.slice(0,15).forEach(url => {
+        const img = document.createElement("img")
+        img.src = url
+        galeria.appendChild(img)
+      })
+}
+
+botaoBuscar.addEventListener('click', exibirImagens)
